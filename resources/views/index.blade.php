55224b06b7d26462786716c0ca6563dd78dd0f9d
twkhjl <twkhjl@gmail.com>
2020-01-22 21:59:54 +0800

    modify todo list fron-end view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,49 +1,48 @@
 @extends('layouts.app')
 @section('content')
-<h2>To-do list</h2>
-    @if (count($todos)>0)
-        @foreach ($todos as $item)
-            <div>title:
-              {{ $item['title'] }}
-            </div>
-            <div>content:
-              {{ $item['content'] }}
-            </div>
-            <div>due:
-              {{ $item['due'] }}
-            </div>
-            <hr>
-        @endforeach
+<div class="row">
+<div class="col-sm-12">
+  <h1 class="text-center my-4">To-do list 
+  </h1>
+</div>
+<div class="col-sm-12 my-4">
+  <a href="" class="text-right btn btn-success"><i class="fas fa-plus mr-2"></i>新增待辦事項...</a>
+</div>
+
+  @if (count($todos)>0)
+  @foreach ($todos as $item)
+    <div class="col-sm-4 my-2">
+      <div class="card">
+        <div class="card-body">
+           <h5 class="card-title border-bottom pb-3">
+             {{ $item['title'] }}
+             <a href="javascript:void(0);" class="btn btn-warning float-right">
+              <i class="fas fa-trash-alt"></i>
+            </a>
+             <a href="javascript:void(0);" class="btn btn-info float-right mx-2">
+              <i class="fas fa-pen"></i>
+            </a>
+          </h5>
+           <p class="card-text">{{ $item['content'] }}</p>
+           
+        </div>
+        <div class="card-footer text-muted">
+          {{ $item['due'] }}
+        </div>
+      </div>
+     </div>
+      @endforeach
     @else
         <div>to to list is empty...</div>
     @endif
 
-
-    <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
 </div>
+
+<style>
+  .card{
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+</style>
 
 
 @endsection
