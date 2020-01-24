8c9fd93a89d34fa007b5fbdc9e9f622fc11341d9
twkhjl <twkhjl@gmail.com>
2020-01-24 16:33:14 +0800

    fix url bugs in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title','todo app')</title>
 
-  <link rel="stylesheet" href="/css/app.css">
-  <script src="/js/app.js"></script>
+<link rel="stylesheet" href="{{ url('/css/app.css') }}">
+  <script src="{{ url('/js/app.js') }}"></script>
 
 </head>
 <body class="d-flex flex-column">
